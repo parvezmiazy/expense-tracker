dec33c257051e0f1ac287f06f101b6f62a8286c6
Parvez Ahmed <miazyparvez@gmail.com>
2022-09-23 12:31:42 +0600

    component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { Header } from "./componets/Header.jsx";
+import { Balance } from "./componets/Balance.jsx";
+import { IncomeExpenses } from "./componets/IncomeExpenses.jsx";
+import { TransactionList } from "./componets/TransactionList";
+import { AddTransaction } from "./componets/AddTransaction";
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </>
+  );
 }
 
 export default App;
